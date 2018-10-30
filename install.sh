8d3cde6ac20067fa6a57c9fa361c97367437d2ab
谷磊 <gulei@guleideMacBook-Pro.local>
2018-10-30 15:08:24 +0800

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cd ~
+cd $HOME
 DOTFILES=$HOME/Mac_dotfiles
 
 export http_proxy=http://127.0.0.1:1087
@@ -11,20 +11,27 @@ brew install wget
 echo "Installing zshell..."
 brew install zsh --HEAD
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-git clone https://github.com/zsh-users/zsh-autosuggestions  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-autosuggestions  $HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 chsh -s /bin/zsh `whoami`
 echo "Done!"
 
 echo "Linking Mac_dotfiles..."
+ln -s -f $DOTFILES/zsh/.zshrc.symlink $HOME/.zshrc
 ln -s -f $DOTFILES/vim/.vimrc.symlink $HOME/.vimrc
 mkdir -p $HOME/.config/nvim
 ln -s -f $DOTFILES/vim/.vimrc.symlink $HOME/.config/nvim/init.vim
 ln -s -f $DOTFILES/tmux/.tmux.conf.symlink $HOME/.tmux.conf
-ln -s -f $DOTFILES/zsh/.zshrc.symlink $HOME/.zshrc
 echo "Done!"
+
+echo "Sourcing origin files"
+source $HOME/.zshrc
+tmux source $HOME/.tmux.conf
+echo "Done!"
+
+cd $HOME
 
 echo "brew install..."
 brew install git --HEAD
@@ -47,10 +54,11 @@ echo "Done!"
 
 echo "Pip install..."
 sudo easy_install pip
-pip install --upgrade pip
+pip install --upgrade --user pip
 pip install --user scrapy autopep8
 echo "Done!"
 
+echo "Install Node"
 nvm install --lts
 npm config set registry https://registry.npm.taobao.org
 
@@ -62,9 +70,10 @@ echo "Done!"
 #Vim Plug
 #For vim
 echo "Install vim-plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #For neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "Done!"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
---
+++ test.sh
@@ -0,0 +1,4 @@
+#! /bin/zsh
+cd ~
+source ~/.nvm/nvm.sh
+nvm -v
